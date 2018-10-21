5ee34f154dfb4ee8546defddbdf00b917f5d2281
zaboltar <cletothar@gmail.com>
2018-10-21 15:08:57 -0500

    failedTut

---
+++ FPS jimveg/FPS/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	public int EnemyHealth = 10;
+
+	 void DeductPoints (int DamageAmount) {
+  		EnemyHealth -= DamageAmount;
+ 	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if (EnemyHealth <= 0) {
+   			Destroy(gameObject);
+  		}
+		
+	}
+}
---
+++ FPS jimveg/FPS/Assets/Scripts/GunFire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if(Input.GetButtonDown("Fire1")) {
+   AudioSource gunsound  = GetComponent<AudioSource>();
+   gunsound.Play();
+   GetComponent<Animation>().Play("GunShot");
+  }
+	}
+}
