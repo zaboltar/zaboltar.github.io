7722908f24af446408667a7877cbc706fb678657
zaboltar <cletothar@gmail.com>
2018-12-18 03:17:04 -0500

    9 de avance

---
+++ Infinite Random Side Scroller Jumper GPJ/Infinite SideScroller gpj/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform;
+	public Transform generationPoint;
+	public float distanceBetween;
+	private float platformWidth;
+	public float distanceBetweenMin;
+	public float distanceBetweenMax;
+	private int platformSelector;
+	//public GameObject[] thePlatformSS;
+	private float[] platformWidthSS;
+
+
+	public ObjectPooler[] theObjPools;
+
+	private float minHeight;
+	public Transform maxHeightPoint;
+	private float maxHeight;
+	public float maxHeightChange;
+	private float heightChange;
+
+	// Use this for initialization
+	void Start () {
+
+
+		//platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+
+
+		platformWidthSS = new float[theObjPools.Length];
+
+		for (int i = 0; i < theObjPools.Length; i++) {
+			platformWidthSS[i] = theObjPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
+		}
+
+		minHeight = transform.position.y;
+		maxHeight = maxHeightPoint.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < generationPoint.position.x) {
+			
+			distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
+
+			platformSelector = Random.Range(0, theObjPools.Length);			
+
+			heightChange = transform.position.y + Random.Range(maxHeightChange, -maxHeightChange);
+
+			if (heightChange > maxHeight) {
+				heightChange = maxHeight;
+			} else if (heightChange < minHeight) {
+				heightChange = minHeight;
+			}
+
+			transform.position = new Vector3(transform.position.x + (platformWidthSS[platformSelector]/2) + distanceBetween, heightChange, transform.position.z);
+			
+			
+
+			//Instantiate(/*thePlatform*/thePlatformSS[platformSelector], transform.position, transform.rotation);
+
+			
+			GameObject newPlatform = theObjPools[platformSelector].GetPooledObject();
+
+			newPlatform.transform.position = transform.position;
+			newPlatform.transform.rotation = transform.rotation;
+			newPlatform.SetActive(true);
+
+			transform.position = new Vector3(transform.position.x + (platformWidthSS[platformSelector]/2), transform.position.y, transform.position.z);
+			
+		}
+	}
+}
---
+++ Infinite Random Side Scroller Jumper GPJ/Infinite SideScroller gpj/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public Text scoreText;
+	public Text highScoreText;
+
+	public float scoreCount;
+	public float highScoreCount;
+
+	public float pointsPerSecond;
+
+	public bool scoreIncreasing; 
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.HasKey("Highscore") != null){
+			highScoreCount = PlayerPrefs.GetFloat("Highscore");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (scoreIncreasing){
+			scoreCount += pointsPerSecond * Time.deltaTime;
+		}
+
+		
+
+		if (scoreCount > highScoreCount) {
+			highScoreCount = scoreCount;
+			PlayerPrefs.SetFloat("Highscore", highScoreCount);
+		}
+		
+		scoreText.text = "Score: " + Mathf.Round(scoreCount) ;
+		highScoreText.text = "Highscore: " + Mathf.Round(highScoreCount) ;
+
+
+	}
+
+	public void AddScore (int pointToAdd) {
+		scoreCount += pointToAdd;
+	}
+
+}
