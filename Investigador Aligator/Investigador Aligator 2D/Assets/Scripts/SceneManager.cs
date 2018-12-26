53091e75c244dc34fb2646e0b65e70478ad0d8a5
zaboltar <cletothar@gmail.com>
2018-12-26 03:43:31 -0500

    inv ali

---
+++ Investigador Aligator/Investigador Aligator 2D/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour {
+
+	public void SceneLoader(int SceneIndex) {
+
+       
+	    //SceneManager.LoadScene(SceneIndex);
+	    UnityEngine.SceneManagement.SceneManager.LoadScene(SceneIndex);
+
+	}
+
+}
